46bbef494093c1a608e900ba6b0208e69604b7d6
achan24 <58598070+achan24@users.noreply.github.com>
2025-02-17 22:47:40 +0000

    initialise project, create app, models, view

---
+++ ecomm/shop/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+  name = models.CharField(max_length=255)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  image = models.ImageField(upload_to='/products')
+
+class Order(models.Model):
+  user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+  total_cost = models.DecimalField(max_digits=10, decimal_places=2)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+class OrderItem(models.Model):
+  order = models.ForeignKey(Order, on_delete=models.CASCADE)
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+  quantity = models.IntegerField()
+
